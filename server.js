2374a218d78cfe27c2bd4f1595c87d6ee42b3c07
cheshahith <cheshahith1106@gmail.com>
2025-03-19 09:56:18 +0530

    Move server to the root directory

---
+++ server.js
@@ -0,0 +1,29 @@
+// Import required modules
+require("dotenv").config(); // Load environment variables from .env
+const express = require("express");
+const { connectDatabase, getConnection } = require("./database");
+
+// Initialize Express app
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Connect to MongoDB
+connectDatabase();
+
+app.get("/", async (req, res) => {
+  res.send(`welcome to the server`);
+});
+// Define a simple home route to check DB status
+app.get("/", async (req, res) => {
+  res.send(`Database Connection Status: ${getConnection()}`);
+});
+
+// Define PORT from environment variables or default to 3000
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
